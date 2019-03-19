a89c6a8225bb5bba2ec790f557cc89f468a7a9d4
Shreyans Jain <shreyansj551@gmail.com>
2019-03-19 10:16:22 +0530

    Basic functionality added

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let costaanName = document.querySelector(".name");
+let department = document.querySelector(".department"); 
+let counter = 0;
+
+const names = ['Johny English', 'Tom Clancy', 'Harry Potter'];
+console.log(names);
+// console.log(names.length);
+costaanName.innerHTML = names[counter];
+
+const nextPerson = () => {
+    if (counter > names.length - 2) 
+        counter = 0;
+    else 
+        counter++;
+    // console.log(counter);
+    costaanName.innerHTML = names[counter];
+}
+
+const prevPerson = () => {
+    if (counter <= 0) 
+        counter = names.length-1;
+    else 
+        counter--;
+    // console.log(counter);
+    costaanName.innerHTML = names[counter];
+}
+
+document.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+        case 39:
+        case 68:
+            nextPerson();
+            break;
+        
+        case 37:
+        case 65:
+            prevPerson();
+            break;
+    }
+})
