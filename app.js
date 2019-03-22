10cb3ebb713b8d871eba8e5b87eda624c24b7ce3
Shreyans Jain <shreyansj551@gmail.com>
2019-03-22 12:14:39 +0530

    Completed animations,added number mapping

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let costaanName = document.getElementsByClassName("name")[0];
 let departmentName = document.getElementsByClassName("department")[0]; 
 
-const names = ['Johny English'];
-const departments = ['Department of Something'];
+const names = ['Johny English', 'Tom Clancy', 'Harry Potter'];
+const departments = ['Department of Something', 'Department of Else', 'Something Else'];
+let counter = 0;
 
 function printLetters(index, arr, nameToPrint) 
 {
@@ -31,14 +32,108 @@ function printLetters(index, arr, nameToPrint)
     }
 }
 
+function addAnimation() {
+    document.getElementsByClassName("th")[0].classList.remove("collapse");
+    document.getElementsByClassName("outer-circle")[0].classList.add("animate-outer-circle");
+    document.getElementsByClassName("inner-circle")[0].classList.add("animate-inner-circle");
+    document.getElementsByClassName("innermost")[0].classList.add("animate-innermost");
+    document.getElementsByClassName("rotator-1")[0].classList.add("animate-rotator");        
+    // document.querySelector(".circles").style.animation = "slide 1s ease forwards";
+} 
+
+function removeAnimation() {
+    document.getElementsByClassName("outer-circle")[0].classList.remove("animate-outer-circle");
+    document.getElementsByClassName("inner-circle")[0].classList.remove("animate-inner-circle");
+    document.getElementsByClassName("innermost")[0].classList.remove("animate-innermost");
+    document.getElementsByClassName("rotator-1")[0].classList.remove("animate-rotator");
+    document.getElementsByClassName("th")[0].classList.add("collapse");
+}
+
+let firstTimeCalled = 0;
+
+function displayDetailsFirst(index) {
+    document.querySelector(".circles").style.animation = "slideLeft 1s ease forwards";
+                
+    setTimeout(()=>{
+        printLetters(index, names, costaanName);
+        printLetters(index, departments, departmentName);
+    },1000)
+    setTimeout(()=> {removeAnimation()},3000)
+    firstTimeCalled = 1;
+}
+
+function displayDetails(index) {
+    addAnimation();
+    while (costaanName.hasChildNodes()) {
+        costaanName.removeChild(costaanName.lastChild);
+    }
+    while (departmentName.hasChildNodes()) {
+        departmentName.removeChild(departmentName.lastChild);
+    }
+
+    setTimeout(()=>{
+        document.querySelector(".circles").style.animation = "slideRight 1s ease forwards";
+    },1000)
+    
+    setTimeout(() => {
+        document.querySelector(".circles").style.animation = "slideLeft 1s ease forwards"; 
+    },2000);
+    
+    setTimeout(()=>{
+        printLetters(index, names, costaanName);
+        printLetters(index, departments, departmentName);
+    },3000)
+    setTimeout(()=>{removeAnimation()},5000)
+}
+
+// window.onload(addAnimation());   
 document.addEventListener("keydown", (e)=> {
-    if (e.keyCode == 32) {
-        console.log('works');
-        document.querySelector(".circles").style.animation = "slide 1s ease forwards";
+    // if (e.keyCode == 32) {
+    //     // console.log('works');
+    //     if (counter == 0) {
+    //         addAnimation();
+    //         counter = 1;
+    //     }
+    //     else {
+    //         removeAnimation();
+    //         counter = 0;
+    //     }
+    // }
+    // // setTimeout()
+    // removeAnimation();
+    // else if (e.keyCode == 65) {
+    //     printLetters(0, names, costaanName);
+    //     printLetters(0, departments, departmentName);
+    // }
+    
+    if (!firstTimeCalled) {
+        switch (e.keyCode) 
+        {
+            case 49: 
+                displayDetailsFirst(0);
+                break;
+            case 50:
+                displayDetailsFirst(1);
+                break;
+            case 51:
+                displayDetailsFirst(2);
+                break;
+        }
     }
-    // setTimeout()
-    else if (e.keyCode == 65) {
-        printLetters(0, names, costaanName);
-        printLetters(0, departments, departmentName);
+
+    else {
+        switch (e.keyCode) 
+        {
+            case 49: 
+                displayDetails(0);
+                break;
+            case 50:
+                displayDetails(1);
+                break;
+            case 51:
+                displayDetails(2);
+                break;
+        }
     }
+    
 })
